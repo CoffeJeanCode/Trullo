ee71dbaec22135499cb77e71c83da2679d261831
erianvc <erianvc.dev@gmail.com>
2020-11-08 20:44:27 -0500

    feat: implements routes

--- packages/frontend/src/App.svelte
+++ packages/frontend/src/App.svelte
@@ -1,7 +1,18 @@
 <script>
-  let msg = "Hello"
+  import { Router, Link, Route } from "svelte-routing";
+  import Home from "./pages/Home.svelte";
+  import Login from "./pages/Login.svelte";
+
+  export const url = null;
 </script>
 
-<main>
-  <h1>{msg}</h1>
-</main>
+<Router {url}>
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="login">Login</Link>
+  </nav>
+  <div>
+    <Route path="login" component="{Login}" />
+    <Route path="/"><Home /></Route>
+  </div>
+</Router>
